8e0a40fcd118840fa91bc4aefb81ab67dd4cf256
Mike Weiblen <mew@mew.cx>
2021-08-29 18:49:12 -0600

    VERY_TEMPORARY/plover/

---
+++ VERY_TEMPORARY/plover/tmp/EXTRACT.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -x
+
+EXE="pdfimages -all -p"
+
+$EXE dutil_dumas.pdf ./dutil_dumas/pic
+$EXE evpatoria07.pdf ./evpatoria07/pic
+$EXE interview01.pdf ./interview01/pic
+$EXE messages.pdf ./messages/pic
+$EXE seti-dutil-dumas.pdf ./seti-dutil-dumas/pic
